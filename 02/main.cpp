9253239924750593e709b747291b17a5b9516f35
Daria Nogina <nogina_daria@mail.ru>
2020-03-16 22:55:33 +0300

    final commit

--- 02/main.cpp
+++ 02/main.cpp
@@ -4,10 +4,23 @@
 void onNumberFound(const char * n){
   std::cout << n << '\n';
 }
+void onStringFound(const char * n){
+  std::cout << "I found a string " << n << '\n';
+}
+void Start(){
+  std::cout << "yay, let's start\n";
+}
+void End(){
+  std::cout << "It's high time to know that it works perfectly\n";
+}
 
 int main()
 {
   register_number_callback(onNumberFound);
+  register_string_callback(onStringFound);
+  register_start_callback(Start);
+  register_end_callback(End);
+
   parse("123 abc 4567");
 
   return 0;
