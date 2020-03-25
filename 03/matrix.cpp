8cfcd75a54e2efe62b1619307d753aa632e2f477
Daria Nogina <nogina_daria@mail.ru>
2020-03-25 23:50:12 +0300

    add 03

---
+++ 03/matrix.cpp
@@ -0,0 +1,60 @@
+#include "matrix.h"
+
+Matrix::Matrix(size_t rows, size_t cols){
+  this->rows = rows;
+  this->cols = cols;
+  this->matrix = new int[rows * cols];
+}
+
+size_t Matrix::getRows(){
+  return this->rows;
+}
+
+size_t Matrix::getColumns(){
+  return this->cols;
+}
+
+Matrix::Proxy::Proxy(Matrix * m, size_t j){
+  this->ncols = m->cols;
+  this->torow = m->matrix + j * this->ncols;
+}
+
+const int& Matrix::Proxy::operator[](size_t j) const{
+  assert(this->ncols > j && "Index Error: index out of range");
+  return this->torow[j];
+}
+
+int& Matrix::Proxy::operator[](size_t j){
+  assert(this->ncols > j && "Index Error: index out of range");
+  return this->torow[j];
+}
+
+Matrix::Proxy::~Proxy() {}
+
+bool Matrix::operator==(const Matrix& i) const{
+  if (this == &i) {
+    return true;
+  }
+  if ((this->cols == i.cols) & (this->rows == i.rows)){
+    for (size_t k=0; k < i.rows * i.cols;k++){
+      if (i.matrix[k] == this->matrix[k]) {}
+    }
+    return true;
+  }
+  return false;
+}
+
+bool Matrix::operator!=(const Matrix& i) const{
+  return !(*this == i);
+}
+
+Matrix& Matrix::operator*=(int p){
+  for (size_t k=0; k < this->rows * this->cols;k++){
+    this->matrix[k] *= p;
+  }
+  return *this;
+}
+
+Matrix::~Matrix(){
+  delete [] matrix;
+}
---
+++ 03/matrix.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cassert>
+#pragma once
+
+class Matrix {
+  size_t rows;
+  size_t cols;
+  int * matrix;
+
+  class Proxy {
+    int * torow;
+    size_t ncols;
+  public:
+    Proxy(Matrix * m, size_t j);
+    const int& operator[](size_t j) const;
+    int& operator[](size_t j);
+    ~Proxy();
+  };
+
+public:
+  Matrix(size_t rows, size_t cols);
+  size_t getRows();
+  size_t getColumns();
+  const Proxy operator[](size_t j) const;
+  Proxy operator[](size_t j);
+  bool operator==(const Matrix& i) const;
+  bool operator!=(const Matrix& i) const;
+  Matrix& operator*=(int p);
+  ~Matrix();
+};
